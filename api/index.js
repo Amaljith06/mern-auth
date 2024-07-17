a6f80148b6b999c706879f7fa366441bd313e763
Amaljith Kumar <amaljithkumar006@gmail.com>
2024-07-17 22:10:48 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 mongoose
@@ -17,6 +19,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
